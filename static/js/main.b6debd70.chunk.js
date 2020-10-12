16f938dff09cf2bb3ca1c7f1eefe02cf0782089b
masyatya <masyatya@gmail.com>
2020-10-12 18:07:53 +0300

    Updates

---
+++ static/js/main.b6debd70.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreact_nav_menu=this.webpackJsonpreact_nav_menu||[]).push([[0],{3:function(e,n,t){e.exports=t(8)},8:function(e,n,t){"use strict";t.r(n);var a=t(0),r=t.n(a),c=t(2);var u=function(){return r.a.createElement("div",{className:"App"},r.a.createElement("h1",null,"Hey"))};t.n(c).a.render(r.a.createElement(u,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.b6debd70.chunk.js.map
